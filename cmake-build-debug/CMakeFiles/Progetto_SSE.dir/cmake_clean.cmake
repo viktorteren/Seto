233a7bb93cea8c7da9b2f1fb6351af96e7474b75
IL_MIO_ROGHINO\Viktor <viktor.teren@gmail.com>
2018-03-26 11:01:09 +0200

    Primo commit

---
+++ cmake-build-debug/CMakeFiles/Progetto_SSE.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Progetto_SSE.dir/main.cpp.o"
+  "Progetto_SSE.pdb"
+  "Progetto_SSE.exe"
+  "libProgetto_SSE.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Progetto_SSE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Progetto_SSE.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Progetto_SSE.
+# This may be replaced when dependencies are built.
