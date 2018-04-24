75b912b03326e2119174ff07e089f56dc68a836a
valentina <vale.napo6@gmail.com>
2018-04-24 19:08:10 +0200

    Regione candidata

--- Label_splitting_module.cpp
+++ Label_splitting_module.cpp
@@ -89,69 +89,158 @@ bool Label_splitting_module::is_bigger_than(Region* region ,set<int>* intersecti
 }
 
 
-vector<Region>* Label_splitting_module::do_label_splitting(map<int, vector<Region*> *>* middle_set_of_states,map<int,vector< pair<int,Region*> >*>* number_of_bad_events){
+vector<Region>* Label_splitting_module::do_label_splitting(map<int, vector<Region*> *>* middle_set_of_states,map<int,vector< int >*>* number_of_bad_events) {
 
     //per ogni evento
     //per ogni stato intermedio se è compreso nel set delle intersezioni
     //prendilo per label splitting
 
-    cout<<"ordine:ptr regions:"<<endl;
-    auto vec_pt= number_of_bad_events->at(2);
 
-    for(auto reg: *vec_pt)
-        cout<<"C: "<< reg.second;
+    vector<int>* events_type=new vector<int>(middle_set_of_states->size());
+    Region* candidate_region= nullptr;
+    int num_bad_event_min=-1;
 
-
-    cout<<"ptr middle set"<<endl;
-    auto middle_vec_ptr=middle_set_of_states->at(2);
-    for(auto reg: * middle_vec_ptr)
-        cout<<" M: "<<reg;
-
-
-    cout<<"middle TOT: " <<endl;
-    vector<Region*>::iterator it;
+    cout << "middle TOT: " << endl;
+    vector<Region *>::iterator it;
     //per ogni evento
-    for(auto e: *middle_set_of_states) {
-        cout<<"EVENTO: "<<e.first<<endl;
-        int pos=0;
+    for (auto e: *middle_set_of_states) {
+        cout << "EVENTO: " << e.first << "*************"<<endl;
+        int pos = 0;
+        candidate_region= nullptr;
+        num_bad_event_min=-1;
+
         //auto set_forced_to_be_a_region;
-        auto event=e.first;
+        auto event = e.first;
         for (it = middle_set_of_states->at(event)->begin(); it < middle_set_of_states->at(event)->end(); ++it) {
-            cout << "middle" << endl;
+            cout << "middle:" << endl;
+            printRegion(**it);
             if (is_bigger_than(*it, regions_intersection->at(event))) {
                 cout << "erase" << endl;
-                *middle_set_of_states->at(event)->erase(it, middle_set_of_states->at(event)->end());
-            }
-            else { //lo stato mi va bene
+                *middle_set_of_states->at(event)->erase(it, it);
+            } else { //lo stato mi va bene
                 //cache e ricalcolo solo se non c'è(avevo fatto break per no cross) mi prendo una coppia evento che viola e vettore di
                 // transazioni e anche il numero di eventi che violano per ogni set di stati!!
-                cout<<"eveent: " <<event << endl;
-                auto vec_ptr= number_of_bad_events->at(event);
-                auto pair= (*vec_ptr)[pos];
-                if( pair.first ==-1 ) {
+                cout << "eveent: " << event << endl;
+                auto vec_ptr = number_of_bad_events->at(event);
+                if ((*vec_ptr)[pos] == -1) {
+
+                    printRegion( **it );
                     //ricalcola
+                    for(auto e  : *middle_set_of_states)
+                        (*events_type)[e.first]=branch_selection(&(ts_map->at(e.first)),*it,e.first);
+
+                    set_number_of_bad_events(events_type,event, vec_ptr+pos);
                 }
 
+                if( (*vec_ptr)[pos]!=0 ){
+                    if(candidate_region==nullptr || num_bad_event_min>(*vec_ptr)[pos]){
+
+                        candidate_region= *it;
+                        num_bad_event_min=(*vec_ptr)[pos];
+
+                    }else if(num_bad_event_min==(*vec_ptr)[pos]){
+                        //controlla in base alla size delle 2 regioni
+                        if(candidate_region->size()>= (*it)->size()){
+                            candidate_region= *it;
+                            num_bad_event_min=(*vec_ptr)[pos];
+                        }
+                    }
+                }
+
+                cout<<"NUMBER candidate: "<< num_bad_event_min<<endl;
+
+                //qui una volta scenlto il candidato fai splitting
             }
-            for (auto el:**it)
-                cout << "S: " << el << endl;
 
             pos++;
-        }
-
-        cout << "intesection" << endl;
-        for (auto inte: *regions_intersection->at(event)) {
-            cout << "S:" << inte << endl;
         }
 
 
         // aggiungo set_forced_to_be_a_region alle regioni del mio evento
         //e splitto l'etichetta(modifico il grafo originale?)
+        cout<<"___________________________REGIONE CANDIDATA__________________"<<endl;
+        if(candidate_region!= nullptr)
+            print(*candidate_region);
+        cout<<"num: " << num_bad_event_min<<endl;
 
 
     }
+}
+int Label_splitting_module::branch_selection(List_edges *list, Region *region, int event) {
+    // quale ramo devo prendere tra ok, nocross oppure 2 rami? (per un evento)
+    vector<int> *trans = new vector<int>(4, 0);
+    cout<<"DENTRO"<<endl;
 
+    //num in-out-exit-enter
+    const int in = 0;
+    const int out = 1;
+    const int exit = 2;
+    const int enter = 3;
 
+    for (auto t: *list) {
+        if (region->find(t.first) != region->end()) { //il primo stato appartiene alla regione
+            if (region->find(t.second) != region->end()) { //anche il secondo stato appartiene alla regione
+                (*trans)[in]++;
+                cout << t.first << "->" << t.second << " IN " << endl;
+                //per no cross è ok, gli altri non si possono fare
+            } else {
+                (*trans)[exit]++;
+                cout << t.first << "->" << t.second << " EXIT" << endl;
+            }
+        } else {//il primo non ci appartiene
+            if (region->find(t.second) != region->end()) { //il secondo stato appartiene alla regione
+                (*trans)[enter]++;
+                cout << t.first << "->" << t.second << " ENTER" << endl;
+            } else {
+                (*trans)[out]++;
+                cout << t.first << "->" << t.second << " OUT" << endl;
+            }
+        }
+    }
 
+    int it = 0;
+    cout << ">> IN = 0/OUT = 1/EXIT = 2/ENTER = 3" << endl;
+    for (auto i: *trans) {
+        cout << "num trans " << it << ": " << i << endl;
+        it++;
+    }
 
+    //gli Enter+in devono diventare per forza in(nocross)
+    if (((*trans)[in] > 0 && (*trans)[enter] > 0) || ((*trans)[in] > 0 && (*trans)[exit] > 0) ||
+        ((*trans)[enter] > 0 && (*trans)[exit] > 0)) {
+        cout << "return no cross" << endl;
+        delete trans;
+        return NOCROSS;
+    } else if ((*trans)[exit] > 0 && (*trans)[out] > 0) { //(exit-out)
+        cout << "return exit_no cross" << endl;
+        delete trans;
+        return EXIT_NOCROSS;
+    } else if ((*trans)[enter] > 0 && (*trans)[out] > 0) { //(enter-out)
+        cout << "return enter_no cross" << endl;
+        delete trans;
+        return ENTER_NOCROSS;
+    } else {
+        cout << "return ok" << endl;
+        delete trans;
+        return OK;
+    }
+
+}
+
+void Label_splitting_module::set_number_of_bad_events(vector<int>* event_type,int event, vector<int> * number_of_bad_events){
+    //conta per ogni set di stati gli eventi bad
+    // pair<int,Region*> *bad_events=new pair<int,Region*>;
+
+    cout<<"SET BAD NUMBER per " <<event<<"********"<<endl;
+
+    int counter=0;
+    for(auto n: *event_type) {
+        if (n != OK ) {
+            counter++;
+        }
+    }
+
+    (*number_of_bad_events)[0]=counter;
+
+    cout<<"COUNTER: " <<counter<<endl;
 }
--- Label_splitting_module.h
+++ Label_splitting_module.h
@@ -27,7 +27,7 @@ public:
     Label_splitting_module(map<int, vector<Region> *>* regions,vector<ER> *er_set);
     ~Label_splitting_module();
     bool is_excitation_closed();
-    vector<Region>* do_label_splitting(map<int, vector<Region*> *>* middle_set_of_states, map<int,vector< pair<int,Region*> >*>* number_of_bad_events);
+    vector<Region>* do_label_splitting(map<int, vector<Region*> *>* middle_set_of_states, map<int,vector< int >*>* number_of_bad_events);
 
 private:
     map<int, vector<Region> *> * regions;
@@ -38,6 +38,8 @@ private:
     map<int, set<int>* > *regions_intersection;
     //coppia numero eventi che violano la regione e ptr_regione
     //map<int,vector< pair<int,Region*> >*>* number_of_bad_events;
+    int branch_selection(List_edges *list, Region *region, int event);
+    void set_number_of_bad_events(vector<int>* event_type,int event,vector< int >* number_of_bad_events);
 
 };
 
