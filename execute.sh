4057a91d30c74e7fc6ba7df5f45a5b9df501a47d
viktor <viktor.teren@gmail.com>
2018-07-04 12:12:24 +0200

    update script per la generazione dei file ECTS.ps

--- execute.sh
+++ execute.sh
@@ -4,19 +4,18 @@ then
     echo "The script needs an input argument corresponding to the path of the TS"
 else
 param=$1
-echo "param = $param" 
 new_end="_PN.dot"
 new_output_end="_PN.ps"
+new_ects="_ECTS.dot"
+new_ects_output_end="_ECTS.ps"
 ts="${param/.ts/.dot}"
-ts="${param/.apt/.dot}"
-echo "ts = $ts" 
 ts_output="${ts/.dot/.ps}"
-echo "ts_output = $ts_output"
 new="${ts/.dot/$new_end}"
-echo "new = $new"
+ects="${ts/.dot/$new_ects}"
+ects_ps="${ts/.dot/$new_ects_output_end}"
 new_ps="${ts/.dot/$new_output_end}"
-echo "new_ps = $new_ps"
 ./cmake-build-debug/Progetto_SSE $1
 dot -T ps $ts -o $ts_output
+dot -T ps $ects -o $ects_ps
 dot -T ps $new -o $new_ps
 fi
