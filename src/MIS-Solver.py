3975c9586d2ee7ab06458762be4c25864a340504
viktor <viktor.teren@gmail.com>
2019-06-27 17:29:48 +0200

    Ptyhon MIS-Solver done

--- src/MIS-Solver.py
+++ src/MIS-Solver.py
@@ -1,38 +1,34 @@
 import networkx as nx
-import matplotlib.pyplot as plt
+import copy
 #READ THE DATA RELATED TO THE GRAPH -> REGIONS AND OVERLAPS BETWEEN REGIONS AND CREATE THE GRAPH WITH THE LIBRARY
 f = open("Graph.dimacs", "r")
-#print(f.readline())
-str = f.readline().split()
-num_vertices = str[1]
-num_edges = str[2]
-
+line = f.readline().split()
+num_vertices = line[1]
+num_edges = line[2]
 
 G = nx.Graph()
+Reduced_G = nx.Graph()
 #set values from 1 to number of regions
-G.add_nodes_from([1, str[1]])
+G.add_nodes_from(range(1, int(num_vertices)+1))
+Reduced_G.add_nodes_from(range(1, int(num_vertices)+1))
 
 #create edges
 for x in f:
     temp = x.split()
-    G.add_edge(temp[1], temp[2])
+    G.add_edge(int(temp[1]), int(temp[2]))
+    Reduced_G.add_edge(int(temp[1]), int(temp[2]))
 
-#plt.subplot(121)
-#nx.draw(G, with_labels=False)
-
-#G = nx.petersen_graph()
-#plt.subplot(121)
-#nx.draw(G, with_labels=True, font_weight='bold')
-#plt.subplot(122)
-#nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
+f.close()
 
 #FIND ALL FSMS (2 STEPS)
-Reduced_G = G
+#Reduced_G = copy.copy(G)
 
 first_FSM = nx.maximal_independent_set(G)
 other_incomplete_FSMs = []
+for node in first_FSM:
+    Reduced_G.remove_node(node)
 
-while (list(Reduced_G.nodes) > 0):
+while (len(list(Reduced_G.nodes)) > 0):
     FSM = nx.maximal_independent_set(Reduced_G)
     other_incomplete_FSMs.append(FSM)
     for node in FSM:
@@ -41,10 +37,16 @@ while (list(Reduced_G.nodes) > 0):
 final_FSMs = []
 final_FSMs.append(first_FSM)
 for FSM in other_incomplete_FSMs:
+    #FSM_int = map(int, FSM)
     final_FSMs.append(nx.maximal_independent_set(G, FSM))
 
 #WRITE A FILE WITH THE NUMBER OF FSMS AND THEIR NODES
 f = open("final_FSMs.txt", "w")
-#f.write(final_FSMs.count())
-f.write("Woops! I have deleted the content!")
+f.write(str(len(final_FSMs)))
+f.write('\n')
+for FSM in final_FSMs:
+    for region in FSM:
+        f.write(str(region)+' ')
+    f.write('\n')
+
 f.close()
