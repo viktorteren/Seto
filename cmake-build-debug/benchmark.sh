0b9c561c3b76d21154e7e7808df356b6e8601c2e
viktor <viktor.teren@gmail.com>
2019-07-04 15:30:14 +0200

    Benchmark script fix

--- cmake-build-debug/benchmark.sh
+++ cmake-build-debug/benchmark.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-exe=/cmake-build-debug/TS_splitter
-bench_dir=./benchmark_dir/*
+bench_dir=../benchmark_dir/*
 for file in $bench_dir
 do
     if [ ${file: -2} == ".g" ]
     then
-        ./cmake-build-debug/TS_splitter "$file" M
+        ./TS_splitter "$file" M
     fi
 done
 
